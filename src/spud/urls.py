b8ea86caeb6a3dd3a8099a4fee49f8ccdf3e1753
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-09-15 12:06:08 +0200

    Add framework for URL rewriting

---
+++ src/spud/urls.py
@@ -0,0 +1,36 @@
+import abc
+
+import urllib.parse
+
+
+class UrlProcessor(abc.ABC):
+    @abc.abstractmethod
+    def process(self, url: urllib.parse.ParseResult) -> urllib.parse.ParseResult:
+        """
+        Takes in a URL, checks whether it needs to be sanitized, and returns a processed version of it.
+
+        If no processing is needed, returns the original URL. See ExampleUrlProcessor in the `test_urls.py` file for an example implementation.
+        """
+        pass
+
+
+class YouTubeUrlProcessor(UrlProcessor):
+    ...
+
+
+class InstagramUrlProcessor(UrlProcessor):
+    ...
+
+
+URL_PROCESSORS = [
+    # YouTubeUrlProcessor(),
+    # InstagramUrlProcessor(),
+]
+
+
+def process_url(url: urllib.parse.ParseResult) -> urllib.parse.ParseResult:
+    """Runs the URL through all registered processors, sequentially."""
+    for processor in URL_PROCESSORS:
+        url = processor.process(url)
+
+    return url
