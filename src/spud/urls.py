319754a76272ddc3331e205255d68e3d31605c34
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-09-15 12:13:21 +0200

    Update docstrings

--- src/spud/urls.py
+++ src/spud/urls.py
@@ -4,22 +4,29 @@ import urllib.parse
 
 
 class UrlProcessor(abc.ABC):
+    """
+    Takes in a URL, checks whether it needs to be sanitized, and returns a processed version of it.
+
+    If no processing is needed, returns the original URL. See ExampleUrlProcessor in the `test_urls.py` file for an example implementation.
+    """
+
     @abc.abstractmethod
     def process(self, url: urllib.parse.ParseResult) -> urllib.parse.ParseResult:
-        """
-        Takes in a URL, checks whether it needs to be sanitized, and returns a processed version of it.
-
-        If no processing is needed, returns the original URL. See ExampleUrlProcessor in the `test_urls.py` file for an example implementation.
-        """
         pass
 
 
 class YouTubeUrlProcessor(UrlProcessor):
-    ...
+    """Takes in a YouTube URL, and ..."""
+
+    def process(self, url: urllib.parse.ParseResult) -> urllib.parse.ParseResult:
+        ...
 
 
 class InstagramUrlProcessor(UrlProcessor):
-    ...
+    """Takes in an Instagram URL, and ..."""
+
+    def process(self, url: urllib.parse.ParseResult) -> urllib.parse.ParseResult:
+        ...
 
 
 URL_PROCESSORS: list[UrlProcessor] = [
